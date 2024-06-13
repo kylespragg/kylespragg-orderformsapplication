36925a4c6a1c78b6d2da0b469f3472a10f053989
Kyle Spragg <kylesspragg@gmail.com>
2024-06-13 16:56:25 -0500

    Revert "Merge branch 'main' of https://github.com/kylespragg/kylespragg-orderformsapplication"
    This reverts commit b6a275559618bda0223d05478bd2e330e2d7b8c8, reversing
    changes made to afc07aca2c342d7253db371686e6fa0521828322.

--- orderformsgfd/server/api/dbConfig.js
+++
@@ -1,7 +0,0 @@
-const knex = require('knex'); //requires functionality of knex
-
-const config = require('../knexfile'); //requires development
-
-const db = knex(config.development); // we are using only development
-
-module.exports = db;
--- orderformsgfd/server/index.js
+++
@@ -1,6 +0,0 @@
-const server = require('./api/server');
-
-const HOST = 'localhost';
-const PORT = 3001;
-
-server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`)); //takes an argument and a callback
